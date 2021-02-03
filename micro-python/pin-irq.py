ba56f15d23f4dc0c54f5ffe9aa1cd3ba8f86b217
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2021-02-03 21:00:15 +0000

    - fixed

--- micro-python/pin-irq.py
+++ micro-python/pin-irq.py
@@ -1,4 +1,3 @@
-# TODO  test
 import time
 from machine import Pin
 import rp2
@@ -6,15 +5,15 @@ import rp2
 @rp2.asm_pio(set_init = rp2.PIO.OUT_LOW)
 def blink_1hz():
     # cycles: 1 + 1 + 6 + 32 * (30 + 1) = 1000
-    irq(rel(0))
-    set(pins, 1)
+    irq(0)         # raises interrupt in StateMachine
+    set(pins, 1)   # raises rising edge interrupt in Pin
     set(x, 31)     [5]
     label("delay_high")
     nop()          [29]
     jmp(x_dec, "delay_high")
     
     # cycles: 1 + 7 + 32 * (30 + 1) = 1000
-    set(pins, 0)
+    set(pins, 0)   # raises falling edge interrupt in Pin
     set(x, 31)     [6]
     label("delay_low")
     nop()          [29]
@@ -22,11 +21,25 @@ def blink_1hz():
 
 sm = rp2.StateMachine(0, blink_1hz, freq = 2000, set_base = Pin(25))
 
-# set the IRQ handler to print the millisecond timestamp
-p25 = Pin(25, Pin.IN, Pin.PULL_UP)
-p25.irq(lambda pin: print("IRQ with flags:", pin.irq().flags in.IRQ_FALLING))
+print("Time  Src   (flags, trigger)")
 
-#sm.irq(lambda p: print(int(time.ticks_ms()/1000)))
+# flags:
+#   IRQ_RISING  --> 8
+#   IRQ_FALLING --> 4
+LED = Pin(25)
+LED.irq(lambda pin:
+    print(
+        "{0:5d}".format(int(time.ticks_ms() / 1000)),
+        "IRQ  ",
+        (pin.irq().flags(), pin.irq().trigger())))
+
+# flags:
+#   OUT  --> 1
+sm.irq(lambda p:
+    print(
+        "{0:5d}".format(int(time.ticks_ms() / 1000)),
+        "SM   ",
+        (p.irq().flags(), p.irq().trigger())))
 
 # start the StateMachine
 sm.active(1)
